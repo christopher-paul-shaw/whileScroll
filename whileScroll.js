920b7a2a2bffe6225a17ec62f932e87d5a42de79
Christopher Shaw <chris@chris-shaw.com>
2018-02-23 10:19:09 +0000

    Create whileScroll.js

---
+++ whileScroll.js
@@ -0,0 +1,14 @@
+		var scrolling = false;
+		window.onscroll = whileScrolling;
+		function whileScrolling() {
+		    scrolling = true;
+		}
+
+		setInterval(function() {
+		    if(scrolling) {
+		        scrolling = false;
+
+		        // Code To Run
+            console.log('hello world');
+		    }
+		}, 500);
